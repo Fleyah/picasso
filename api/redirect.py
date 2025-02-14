f71092be90b5da10b3f02cd89a9630c6a7c21054
Fleyah <167648103+Fleyah@users.noreply.github.com>
2025-02-14 22:57:17 +0100

    Update redirect.py

--- api/redirect.py
+++ api/redirect.py
@@ -1,1 +1,9 @@
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+
+app = FastAPI()
+
+@app.get("/")
+def redirect_to_scarletta():
+    return RedirectResponse("https://giffy-lake-three.vercel.app/api/image.py")
 
